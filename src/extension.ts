d88af287ab706f02bce7ca6b1bf58aa58ddd933a
Vũ Quốc Đạt <datvu@vinades.vn>
2025-08-31 10:48:27 +0700

    Chuẩn bị đóng gói

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "xtemplate-block" is now active!');
+	console.log('Hello Xtemplate!');
 	
 	// Khởi tạo để dùng cho cả 3 chỗ
 	const Symbols = new XTemplateDocumentSymbolProvider();
@@ -33,17 +33,6 @@ export function activate(context: vscode.ExtensionContext) {
 		{ language: 'html' },
 		new XTemplateHoverProvider( Symbols )
 	);
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('xtemplate-block.helloDat', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello Dat Fit');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is DEACTIVATED
